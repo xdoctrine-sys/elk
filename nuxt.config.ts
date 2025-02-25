9c8dd35a216e3ec39a57524844c592ab64f3ab9b
xdoctrine-sys <qatar.jsx@gmail.com>
2025-02-25 14:40:25 +0300

    d
    d

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -264,14 +264,14 @@ export default defineNuxtConfig({
       meta: [
         { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
         // open graph social image
-        { property: 'og:title', content: 'Elk' },
-        { property: 'og:description', content: 'A nimble Mastodon web client' },
+        { property: 'og:title', content: 'Yulup' },
+        { property: 'og:description', content: 'A Social Media web client' },
         { property: 'og:type', content: 'website' },
         { property: 'og:image', content: 'https://elk.zone/elk-og.png' },
         { property: 'og:image:width', content: '3800' },
         { property: 'og:image:height', content: '1900' },
-        { property: 'og:site_name', content: 'Elk' },
-        { name: 'twitter:site', content: '@elk_zone' },
+        { property: 'og:site_name', content: 'Yulup' },
+        { name: 'twitter:site', content: '@Yulup_zone' },
         { name: 'twitter:card', content: 'summary_large_image' },
       ],
     },
